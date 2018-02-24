f27a3772676421bfed87a2e6a8d7761c7e14c5ef
charlesXu86 <charlesxu86@163.com>
2018-02-24 17:10:38 +0800

    Xgboost调优示例.py

---
+++ Loan_risk_prediction/code/Xgboost调优示例.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Xgboost调优示例.py
+@time: 2018/2/24 17:09
+@desc:
+"""
+
+
+
+
+if __name__ == '__main__':
+    pass
