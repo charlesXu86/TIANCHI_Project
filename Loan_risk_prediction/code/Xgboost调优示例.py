95738f93ae1ab3972c6e48058e1217a4badb276d
charlesXu86 <charlesxu86@163.com>
2018-02-25 10:08:24 +0800

    Xgboost调优示例

--- Loan_risk_prediction/code/Xgboost调优示例.py
+++ Loan_risk_prediction/code/Xgboost调优示例.py
@@ -8,6 +8,23 @@
 @time: 2018/2/24 17:09
 @desc:
 """
+'''
+   Xgboost 调参详解
+'''
+from xgboost import XGBClassifier
+
+#
+# 第一步: 确定学习速率和tree_based参数  调优的估计器的数目
+#
+xgb1 = XGBClassifier(
+    learning_rate= 0.1,
+    n_estimators= 1000,
+    max_depth= 5,
+    min_child_weight= 1,
+    gamma= 0,
+    subsample= 0.8,
+
+)
 
 
 
