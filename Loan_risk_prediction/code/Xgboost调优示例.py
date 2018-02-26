9da3e7e966aec6461c2c8ff42990ec42aab0f88f
charlesXu86 <charlesxu86@163.com>
2018-02-26 18:53:39 +0800

    Xgboost调优示例

--- Loan_risk_prediction/code/Xgboost调优示例.py
+++ Loan_risk_prediction/code/Xgboost调优示例.py
@@ -23,6 +23,7 @@ import matplotlib.pylab as plt
 from matplotlib.pylab import rcParams
 from xgboost import XGBClassifier
 from sklearn import svm, grid_search, datasets
+from sklearn import cross_validation, metrics
 from sklearn.model_selection import GridSearchCV  # 网格搜索
 
 # %matplotlib inline
@@ -37,7 +38,25 @@ def modelfit(alg, dtrain, predictors, useTrainCV=True, cv_folds=0.5, early_stopp
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
         xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
-        cvresult = xgb.cv(xgb_param, xgtrain)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'],
+                         nfold=cv_folds, metrics='auc', early_stopping_rounds=early_stopping_rounds,
+                          show_progress=False)
+        alg.set_params(n_estimators=cvresult.shape[0])
+    # Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain['Disbursed'], eval_metric='auc')
+
+    # Predict training set
+    dtrain_predictions = alg.predict(dtrain[predictors])
+    dtrain_predprob = alg.predict_prob(dtrain[predictors])[:, 1]
+
+    print('\n Model Report')
+    print('Accuracy: %.4g' % metrics.accuracy_score(dtrain['Disbursed'].values, dtrain_predictions))
+    print('AUC Score (Train): %f' % metrics.roc_auc_score(dtrain['Disbursed'], dtrain_predprob))
+
+    feat_imp = pd.Series(alg.booster().get_fcore()).sort_values(ascending=False)
+    feat_imp.plot(kind='bar', title='Feature Importance')
+    plt.ylabel('Feature Importance')
+
 
 #
 # 第一步: 确定学习速率和tree_based参数  调优的估计器的数目
@@ -123,6 +142,29 @@ gsearch1.fit(train[predictors],train[target])
 gsearch1.grid_scores_,
 gsearch1.best_params_,
 gsearch1.best_score_
+
+# 第三步: gamma参数调优
+param_test3 = {
+    'gamma':[i/10.0 for i in range(0, 5)]
+}
+gsearch3 = GridSearchCV(estimator=XGBClassifier(
+    learning_rate=0.1,
+    n_estimators=140,
+    max_depth=4,
+    min_child_weight=6,
+    gamma=0,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    objective='binary:logistic',
+    nthread=4,
+    scale_pos_weight=1,
+    seed=27),
+    param_grid=param_test3,
+    scoring='roc_auc',
+    n_jobs=4,
+    iid=False,
+    cv=5
+)
 
 
 
