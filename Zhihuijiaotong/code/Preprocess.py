73b3cba0e2487adf6af2f95bc9d2913d533bd75b
charlesXu86 <charlesxu86@163.com>
2018-02-27 13:15:04 +0800

    数据预处理（类型转换，缺失值处理，特征提取）

---
+++ Zhihuijiaotong/code/Preprocess.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Preprocess.py
+@time: 2018/2/27 13:14
+@desc: 数据预处理（类型转换，缺失值处理，特征提取）
+"""
+
+if __name__ == '__main__':
+    pass
