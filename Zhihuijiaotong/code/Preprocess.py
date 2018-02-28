7f80c27f09bec7f16f580117720f3e1c83dbb585
charlesXu86 <charlesxu86@163.com>
2018-02-28 14:29:41 +0800

    数据预处理（类型转换，缺失值处理，特征提取）

--- Zhihuijiaotong/code/Preprocess.py
+++ Zhihuijiaotong/code/Preprocess.py
@@ -50,6 +50,66 @@ def cast_log_outliers(to_file):
 
 def imputation_prepre(file, to_file):
     df = pd.read_csv(file, delimiter=';', parse_dates=['time_interval_begin'], dtype={'link_ID':object})
+    link_df = pd.read_csv('E:\dataset\data\gy_contest_link_info.txt', delimiter=';', dtype={'link_ID':object})
+    date_range = pd.date_range('2017-03-01 00:00:00', '2017-07-31 23:58:00', freq='2min')
+    new_index = pd.MultiIndex.from_product([link_df['link_ID'].unique(), date_range],
+                                              names=['link_ID', 'time_interval_begin'])
+    new_df = pd.DataFrame(index=new_index).reset_index()
+    df2 = pd.merge(new_df, df, on=['link_ID', 'time_interval_begin'], how='left')
+
+    df2 = df2.loc[(df2['time_interval_begin'].dt.hour.isin([6, 7, 8, 13, 14, 15, 16, 17, 18]))]
+    df2 = df2.loc[~((df2['time_interval_begin'].dt.year == 2017) & (df2['time_interval_begin'].dt.month == 7) &
+                  (df2['time_interval_begin'].dt.hour.isin([8, 15, 18])))]
+    df2 =df2.loc[~((df2['time_interval_begin'].dt.year == 2017) & (df2['time_interval_begin'].dt.month == 3) &
+                   (df2['time_interval_begin'].dt.day == 31))]
+
+    df2['date'] = df2['time_interval_begin'].dt.strftime('%Y-%m-%d')
+
+    print(df2.count())
+
+    df2.to_csv(to_file, header=True, index=None, sep=';', mode='w')
+
+def imputation_with_model(file, to_file):
+    '''
+
+    :param file:
+    :param to_file:
+    :return:
+    '''
+    df = pd.read_csv(file, delimiter=';', parse_dates=['time_interval_begin'], dtype={'link_ID':object})
+    print(df.describe())
+
+    link_infos = pd.read_csv('E:\dataset\data\gy_contest_link_info.txt', delimiter=';', dtype={'link_ID': object})
+    link_tops = pd.read_csv('E:\dataset\data\gy_contest_link_top.txt', delimiter=';', dtype={'link_ID': object})
+    link_tops['in_links'] = link_tops['in_links'].str.len().apply(lambda x: np.floor(x / 19))
+    link_tops['out_links'] = link_tops['out_links'].str.len().apply(lambda x: np.floor(x / 19))
+    link_tops = link_tops.fillna(0)
+    link_infos = pd.merge(link_infos, link_tops, on='link_ID', how='left')
+    link_infos['area'] = link_infos['length'] * link_infos['width']
+    link_infos['links_num'] = link_infos['in_links'].astype('str') + "," + link_infos['out_links'].astype('str')
+    df = pd.merge(df, link_infos[['link_ID', 'length', 'width', 'links_num', 'area']], on=['link_ID'], how='left')
+
+    df.loc[df['date'].isin(
+        ['2017-04-02', '2017-04-03', '2017-04-04', '2017-04-29', '2017-04-30', '2017-05-01',
+         '2017-05-28', '2017-05-29', '2017-05-30']), 'vacation'] = 1
+    df.loc[~df['date'].isin(
+        ['2017-04-02', '2017-04-03', '2017-04-04', '2017-04-29', '2017-04-30', '2017-05-01',
+         '2017-05-28', '2017-05-29', '2017-05-30']), 'vacation'] = 0
+
+    df['hour'] = df['time_interval_begin'].dt.hour
+    df['week_day'] = df['time_interval_begin'].map(lambda x: x.weekday() + 1)
+    df['month'] = df['time_interval_begin'].dt.month
+    df['year'] = df['time_interval_begin'].df.year
+
+    df = pd.get_dummies(df, columns=['vocation', 'links_num', 'hour', 'week_day', 'month', 'year'])     # 将分类变量转换为虚拟/指示符变量
+
+    def mean_time(group):
+
+
+
+
+
+
 
 
 
