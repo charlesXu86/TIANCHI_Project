56450b3a5a4dbd875aa370e27807eb5cb6c0a3ec
charlesxu86 <charlesxu86@163.com>
2018-03-02 13:16:50 +0800

    代码模块

---
+++ Ad_Convert_prediction/src/Data_Preprocess.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Data_Preprocess.py
+@time: 2018/3/2 13:15
+@desc: 阿里妈妈广告点击转化数据预处理
+"""
+
+if __name__ == '__main__':
+    pass
