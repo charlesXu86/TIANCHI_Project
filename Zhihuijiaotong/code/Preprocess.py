3c9c644b0b6585ebdc1bbdf72c468e0fe46215f0
charlesxu86 <charlesxu86@163.com>
2018-03-02 10:55:17 +0800

    数据预处理

--- Zhihuijiaotong/code/Preprocess.py
+++ Zhihuijiaotong/code/Preprocess.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import train_test_split
 from scipy.interpolate import UnivariateSpline  # 一维平滑样条拟合一组给定的数据点
 from sklearn import linear_model
 
-pd.options.display.float_format = '{:, .2f}'.format()
+pd.options.display.float_format = '{:, .2f}'.format
 pd.set_option('display.max_rows', 20)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
@@ -160,6 +160,174 @@ def imputation_with_model(file, to_file):
     df['travel_time'] = df['travel_time'].fillna(value=df['prediction'])
     df[['link_ID', 'date', 'time_interval_begin', 'travel_time', 'imputation1']].to_csv(to_file, header=True, index=None, sep=';', mode='w')
 
+def imputation_with_spline(file, to_file):
+    df = pd.read_csv(file, delimiter=';', parse_dates=['time_travel_begin'], dtype={'link_ID':object})
+    df['travel_time2'] = df['travel_time']
+
+    def date_trend(group):
+        tmp = group.groupby('date_hour').mean().reset_index()
+
+        def nan_helper(y):
+            return np.isnan(y), lambda z: z.nonezero()[0]
+
+        y = tmp['travel_time'].values()
+        nans, x = nan_helper(y)
+        if group.link_ID.values[0] in ['3377906282328510514', '3377906283328510514', '4377906280784800514',
+                                       '9377906281555510514']:
+            tmp['date_trend'] = group('travel_time').median()
+        else:
+            regr = linear_model.LinearRegression()
+            regr.fit(x(~nans).reshape(-1, 1), y[~nans].reshape(-1, 1))  # 变成一列
+            tmp['date_trend'] = regr.predict(tmp.index.values.reshape(-1, 1)).ravel()
+        group = pd.merge(group, tmp['date_trend', 'date_hour'], on='date_hour', how='left')
+        return group
+
+    df['date_hour'] = df.time_interval_begin.map(lambda x: x.strftime('%Y-%m-%d-%H'))
+    df = df.groupby('link_ID').apply(date_trend)
+
+    df = df.drop(['date_hour', 'link_ID'], axis=1)
+    df = df.reset_index()
+    df = df.drop('level_1', axis=1)
+    df['travel_time'] = df['travel_time'] - df['date_trend']
+
+    def minute_trend(group):
+        tmp = group.groupby('hour_minute').mean().reset_index()
+        spl = UnivariateSpline(tmp.index, tmp['travel_time'].values, s=0.5, k=3)
+        tmp['minute_trend'] = spl(tmp.index)
+        # plt.plot(tmp.index, spl(tmp.index), 'r', tmp.index, tmp['travel_time'], 'o')
+        # plt.title(group.link_ID.values[0])
+        # plt.show()
+        # print group.link_ID.values[0]
+        group = pd.merge(group, tmp[['minute_trend', 'hour_minute']], on='hour_minute', how='left')
+
+        return group
+
+    df['hour_minute'] = df.time_interval_begin.map(lambda x: x.strftime('%H-%M'))
+    df = df.groupby('link_ID').apply(minute_trend)
+
+    df = df.drop(['hour_minute', 'link_ID'], axis=1)
+    df = df.reset_index()
+    df = df.drop('level_1', axis=1)
+    df['travel_time'] = df['travel_time'] - df['minute_trend']
+
+    link_infos = pd.read_csv('E:\dataset\data\gy_contest_link_info.txt', delimiter=';', dtype={'link_ID': object})
+    link_tops = pd.read_csv('E:\dataset\data\gy_contest_link_top.txt', delimiter=';', dtype={'link_ID': object})
+    link_tops['in_links'] = link_tops['in_links'].str.len().apply(lambda x: np.floor(x / 19))
+    link_tops['out_links'] = link_tops['out_links'].str.len().apply(lambda x: np.floor(x / 19))
+    link_tops = link_tops.fillna(0)
+    link_infos = pd.merge(link_infos, link_tops, on=['link_ID'], how='left')
+    link_infos['links_num'] = link_infos["in_links"].astype('str') + "," + link_infos["out_links"].astype('str')
+    link_infos['area'] = link_infos['length'] * link_infos['width']
+    df = pd.merge(df, link_infos[['link_ID', 'length', 'width', 'links_num', 'area']], on=['link_ID'], how='left')
+
+    df.loc[df['date'].isin(
+        ['2017-04-02', '2017-04-03', '2017-04-04', '2017-04-29', '2017-04-30', '2017-05-01',
+         '2017-05-28', '2017-05-29', '2017-05-30']), 'vacation'] = 1
+
+    df.loc[~df['date'].isin(
+        ['2017-04-02', '2017-04-03', '2017-04-04', '2017-04-29', '2017-04-30', '2017-05-01',
+         '2017-05-28', '2017-05-29', '2017-05-30']), 'vacation'] = 0
+
+    df['minute'] = df['time_interval_begin'].dt.minute
+    df['hour'] = df['time_interval_begin'].dt.hour
+    df['day'] = df['time_interval_begin'].dt.day
+    df['week_day'] = df['time_interval_begin'].map(lambda x: x.weekday() + 1)
+    df['month'] = df['time_interval_begin'].dt.month
+
+    def mean_time(group):
+        group['link_ID_en'] = group['travel_time'].mean()
+        return group
+
+    df = df.groupby('link_ID').apply(mean_time)
+    sorted_link = np.sort(df['link_ID_en'].unique())
+    df['link_ID_en'] = df['link_ID_en'].map(lambda x: np.argmin(x >= sorted_link))
+
+    def std(group):
+        group['travel_time_std'] = np.std(group['travel_time'])
+        return group
+
+    df = df.groupby('link_ID').apply(std)
+    df['travel_time'] = df['travel_time'] = df['travel_time_std']
+
+    params = {
+        'learning_rate': 0.2,
+        'n_estimators': 30,
+        'subsample': 0.8,
+        'colsample_bytree': 0.6,
+        'max_depth': 10,
+        'min_child_weight': 1,
+        'reg_alpha': 0,
+        'gamma': 0
+    }
+
+    df = pd.get_dummies(df, columns=['link_num', 'width', 'minute', 'hour', 'week_day', 'day', 'month'])
+    print(df.head(20))
+
+    feature = df.columns.values.tolist()
+    train_feature = [x for x in feature if
+                    x not in ['link_ID', 'time_interval_begin', 'travel_time', 'date', 'travel_time2', 'minute_trend',
+                              'treval_time_std', 'date_trend']]
+    train_df = df.loc[~df['travel_time'].isnull()]
+    test_df = df.loc[df['travel_time'].isnull()].copy()
+    print(train_feature)
+    X = train_df[train_feature].values
+    y = train_df['travel_time'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+    eval_set = [(X_test, y_test)]
+    regressor = xgb.XGBRegressor(
+        learning_rate=params['learning_rate'],
+        n_estimators=params['n_estimators'],
+        booster='gbtree',
+        objective='reg:linear',
+        n_jobs=-1,
+        subsample=params['subsample'],
+        colsample_bytree=params['colsample_bytree'],
+        random_state=0,
+        max_depth=params['max_depth'],
+        gamma=params['gamma'],
+        min_child_weight=params['min_child_weight'],
+        reg_alpha=params['reg_alpha']
+    )
+    test_df['prediction'] = regressor.predict(test_df[train_feature].values)
+
+    df = pd.merge(df, test_df[['link_ID', 'time_interval_begin', 'prediction']], on=['link_ID', 'time_interval_begin'], how='left')
+
+    feature_vis(regressor,train_feature)
+
+    df['imputation1'] = df['travel_time'].isnull()
+    df['travel_time'] = df['travel_time'].fillna(value=df['prediction'])
+    df['travel_time'] = (df['travel_time'] * np.array(df['travel_time_std']) + np.array(df['minute_trend'])
+                         + np.array(df['date_trend']))
+
+    print(df[['travel_time', 'prediction', 'travel_time2']].describe())
+    df[['link_ID', 'date', 'time_interval_begin', 'travel_time', 'imputation1']].to_csv(to_file, header=True,
+                                                                                        index=None,
+                                                                                        sep=';', mode='w')
+
+def vis_imputation(df):
+    def vis_minute_trend(group):
+        group['travel_time'].plot()
+        tmp = group.loc[group['imputation1'] == True]
+        plt.scatter(tmp.index, tmp['travel_time'], c='r')
+        plt.show()
+
+    def vis_date_trend(group):
+        group.groupby('date_hour').mean()['travel_time'].plot(figsize=(10, 15), title=group.link_ID.values[0])
+        print(group.link_ID.values[0])
+        plt.show()
+
+    # vis imputation for date_trend
+    df['date_hour'] = df.time_interval_begin.map(lambda x: x.strftime('%Y-%m-%d-%H'))
+    df.groupby('link_ID').apply(vis_date_trend)
+
+    # vis imputation for minute_trend
+    df['hour_minute'] = df.time_interval_begin.map(lambda x: x.strftime('%H-%M'))
+    df.groupby(['link_ID', 'date']).apply(vis_minute_trend)
+
+
+# ====
 def create_lagging(df, df_original, i):
     df1 = df_original.copy()
     df1['time_interval_begin'] = df1['time_interval_begin'] + pd.DateOffset(minutes = i * 2)
@@ -214,7 +382,38 @@ def create_feture(file, to_file, lagging=5):
     df2.loc[df2['time_interval_begin'].dt.hour.isin([16, 17, 18]), 'minute_series'] = \
         df2['time_interval_begin'].dt.minute + (df2['time_interval_begin'].dt.hour - 16) * 60
 
+    # day_of_week_en feature
+    df2['day_of_week'] = df2['time_interval_begin'].map(lambda x: x.weekday() + 1)
+    df2.loc[df2['day_of_week'].isin([1, 2, 3]), 'day_of_week_en'] = 1
+    df2.loc[df2['day_of_week'].isin([4, 5]), 'day_of_week_en'] = 2
+    df2.loc[df2['day_of_week'].isin([6, 7]), 'day_of_week_en'] = 3
 
+    # hour_en feature
+    df2.loc[df['time_interval_begin'].dt.hour.isin([6, 7, 8]), 'hour_en'] = 1
+    df2.loc[df['time_interval_begin'].dt.hour.isin([13, 14, 15]), 'hour_en'] = 2
+    df2.loc[df['time_interval_begin'].dt.hour.isin([16, 17, 18]), 'hour_en'] = 3
+
+    # week_hour feature
+    df2['week_hour'] = df2["day_of_week_en"].astype('str') + "," + df2["hour_en"].astype('str')
+
+    # df2.boxplot(by=['week_hour'], column='travel_time')
+    # plt.show()
+
+    df2 = pd.get_dummies(df2, columns=['week_hour', 'link_num', 'width'])
+
+    # ID label encode
+    def mean_time(group):
+        group['link_ID_en'] = group['travel_time'].mean()
+        return group
+
+    df2 = df2.groupby('link_ID').apply(mean_time)
+    sorted_link = np.sort(df2['link_ID_en'].unique())
+    df2['link_ID_en'] = df2['link_ID_en'].map(lambda x: np.argmin(x >= sorted_link))
+
+    print(df2.head(20))
+
+    df2.to_csv(to_file, header=True, index=None, sep=';', mode='w')
 
 if __name__ == '__main__':
+
     create_feture('data/com_training.txt', 'data/training.txt', lagging=5)
