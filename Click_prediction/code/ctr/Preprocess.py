1aadc0559d6e79326854d30c5757f54ab5ab8c1f
charlesxu86 <charlesxu86@163.com>
2018-03-05 13:42:34 +0800

    ctr数据预处理

---
+++ Click_prediction/code/ctr/Preprocess.py
@@ -0,0 +1,26 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Preprocess.py
+@time: 2018/3/5 11:08
+@desc: 数据预处理
+"""
+
+'''
+  生成神经网络的输入
+  生成ffm的输入
+  生成GBDT的输入
+'''
+
+continous_features = range(1, 14)
+categorial_features = range(14, 40)
+
+
+
+
+
+if __name__ == '__main__':
+    pass
