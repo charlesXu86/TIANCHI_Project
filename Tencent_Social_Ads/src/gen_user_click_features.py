23cf7fc861b6f87cc15c7eaa916e365da38c10d8
charlesxu86 <charlesxu86@163.com>
2018-03-08 15:36:20 +0800

    生成用户点击的特征

---
+++ Tencent_Social_Ads/src/gen_user_click_features.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: gen_user_click_features.py
+@time: 2018/3/8 15:35
+@desc: 生成用户点击的特征
+"""
+
+
+
+if __name__ == '__main__':
+    pass
