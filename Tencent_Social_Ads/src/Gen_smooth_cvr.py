dbcf2bf9bf4b6c193ba9317ff86740e33018eb1f
charlesxu86 <charlesxu86@163.com>
2018-03-15 14:55:03 +0800

    广告转化率特征提取

---
+++ Tencent_Social_Ads/src/Gen_smooth_cvr.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Gen_smooth_cvr.py
+@time: 2018/3/15 11:22
+@desc: 广告转化率特征提取
+       构造转化率特征，使用全局和滑动窗口等方式计算单特征转化率，组合特征转化率，使用均值填充，层级填充，贝叶斯平滑，拉普拉斯平滑等方式对转化率进行修正。
+"""
+
+if __name__ == '__main__':
+    pass
