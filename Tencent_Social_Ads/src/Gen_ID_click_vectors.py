be745910f9a83edf99665838bb50a83b3b0fe6b2
charlesxu86 <charlesxu86@163.com>
2018-03-19 17:17:23 +0800

    广告描述向量特征提取

---
+++ Tencent_Social_Ads/src/Gen_ID_click_vectors.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Gen_ID_click_vectors.py
+@time: 2018/3/19 17:16
+@desc: 广告描述向量特征提取
+"""
+
+if __name__ == '__main__':
+    pass
